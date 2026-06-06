37a40de52af11eb9da3fc1b8f9a95597b19cb1f3
Derived <ytdli08@gmail.com>
2026-06-06 15:03:38 -0400

    add

--- zeroshot_post_process.py
+++ zeroshot_post_process.py
@@ -579,7 +579,7 @@ all_df = pd.concat([emotions_df, urgency_df, arxiv_df])
 # %%
 # Toggle if using the machine translated multilingual one
 augmented_df = load_parquet_frame(LOCAL_AUGMENTED_PARQUET, AUGMENTED_PARQUET_URL)
-full_df = augmented_df if False else all_df
+full_df = augmented_df if True else all_df
 
 # %%
 # Baseline analysis (original scores)
